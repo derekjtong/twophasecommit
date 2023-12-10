8134831cf59de40a930181f76e98fd37211ebdfa
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-10 06:48:49 -0500

    Implement commit and rollback

--- node/2pc_coordinator.go
+++ node/2pc_coordinator.go
@@ -32,42 +32,56 @@ func (n *Node) ParticipantCoordinatorSend(req *ParticipantCoordinatorSendRequest
 			errMsg += fmt.Sprintf("Error during prepare phase for %s: %v. ", req.TargetName, errB)
 		}
 		if !okA {
-			errMsg += fmt.Sprintf("%s was not ready to commit. ", req.SenderName)
+			errMsg += fmt.Sprintf("participant %s was not ready to commit. ", req.SenderName)
 		}
 		if !okB {
-			errMsg += fmt.Sprintf("%s was not ready to commit. ", req.TargetName)
+			errMsg += fmt.Sprintf("participant %s was not ready to commit. ", req.TargetName)
 		}
 
-		return fmt.Errorf("transaction aborted. Details: %s", errMsg)
+		return fmt.Errorf("transaction aborted. %s rollback initiated", errMsg)
 	}
 
 	// Step 2: Commit Phase
 	n.Print("Commit phase")
-	n.sendCommit("ParticipantA")
-	n.sendCommit("ParticipantB")
+	n.sendCommit(req.SenderName)
+	n.sendCommit(req.TargetName)
 
 	return nil
 }
 
 // Send prepare request
 func (n *Node) sendPrepare(name string, amount float64) (bool, error) {
-	prepareReq := PrepareRequest{
+	req := ReceivePrepareRequest{
 		Amount: amount,
 	}
-	var prepareRes PrepareResponse
+	var res ReceivePrepareResponse
 	client := n.c_participantClients[name].Client
 
 	// Send Prepare
-	err := client.Call("Node.Prepare", &prepareReq, &prepareRes)
+	err := client.Call("Node.ReceivePrepare", &req, &res)
 	if err != nil {
 		return false, err
 	}
-	return prepareRes.Ready, nil
+	return res.Ready, nil
 }
 
 // Send commit
-func (n *Node) sendCommit(participant string) {
+func (n *Node) sendCommit(name string) {
+	client := n.c_participantClients[name].Client
+	var req ReceiveCommitRequest
+	var res ReceiveCommitResponse
+	err := client.Call("Node.ReceiveCommit", &req, &res)
+	if err != nil {
+		n.Print(fmt.Sprintf("Error sending commit: %v", err))
+	}
 }
 
-func (n *Node) sendRollback(participant string) {
+func (n *Node) sendRollback(name string) {
+	client := n.c_participantClients[name].Client
+	var req ReceiveRollbackRequest
+	var res ReceiveCommitResponse
+	err := client.Call("Node.ReceiveRollback", &req, &res)
+	if err != nil {
+		n.Print(fmt.Sprintf("Error rolling back: %v", err))
+	}
 }
